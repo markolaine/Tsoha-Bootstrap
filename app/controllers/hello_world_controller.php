c9876a611a7263dbc492846be6a6ecb9b385b5b4
Marko Laine <kheevo@gmail.com>
2015-05-12 14:11:44 +0300

    html mallisivut

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,33 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('muistilista.html');
+        //    echo 'Tämä on testihommeli';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+//    public static function sandbox(){
+//      // Testaa koodiasi täällä
+//    //  echo 'Hello World!';
+//        View::make('helloworld.html');
+//    }
+
+    public static function sandbox() {
+        View::make('helloworld.html');
     }
-  }
+
+    public static function listaus() {
+        View::make('listaus.html');
+    }
+
+    public static function uusitehtava() {
+        View::make('uusitehtava.html');
+    }
+
+    public static function login() {
+        View::make('login.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -3,7 +3,22 @@
   $routes->get('/', function() {
     HelloWorldController::index();
   });
+  
+    $routes->get('/muistilista.html', function() {
+    HelloWorldController::index();
+  });
 
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/listaus', function() {
+  HelloWorldController::listaus();
+});
+$routes->get('/uusitehtava', function() {
+  HelloWorldController::uusitehtava();
+});
+
+$routes->get('/login', function() {
+  HelloWorldController::login();
+});
