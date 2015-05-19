79593b91b86b74c197a936b06da3429ff0967b10
Marko Laine <kheevo@gmail.com>
2015-05-19 18:26:31 +0300

    add test data

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,29 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+INSERT INTO users
+(username, password)
+VALUES ('jaska', 'jaska123');
+
+INSERT INTO users
+(username, password)
+VALUES ('miinaharava', 'miinaharava123');
+
+INSERT INTO tasks
+(title, priority, done, added, info)
+VALUES ('Muista vääntää sitä tsohaa', '1', 'false', NOW(), 'Siitä saa kuitenkin muutaman nopan');
+
+INSERT INTO classes
+(classname)
+VALUES ('koti');
+
+INSERT INTO classes
+(classname)
+VALUES ('koulu');
+
+INSERT INTO classes
+(classname)
+VALUES ('tyo');
+
+INSERT INTO classes
+(classname)
+VALUES ('harrastus');
